75daf79347362e6295d7aaf7d3dbf0975bac45dc
OKINAOKINA <43664616+OKINAOKINA@users.noreply.github.com>
2021-07-18 18:29:37 +0900

    Add files via upload

---
+++ XPhysX.cpp
@@ -0,0 +1,308 @@
+﻿//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Copyright (c) 2008-2021 NVIDIA Corporation. All rights reserved.
+// Copyright (c) 2004-2008 AGEIA Technologies, Inc. All rights reserved.
+// Copyright (c) 2001-2004 NovodeX AG. All rights reserved.  
+
+
+#define RENDER_SNIPPET
+
+
+
+#include <vector>
+
+#include "PxPhysicsAPI.h"
+
+#include "../snippets/snippetrender/SnippetRender.h"
+#include "../snippets/snippetrender/SnippetCamera.h"
+
+#ifdef RENDER_SNIPPET
+
+using namespace physx;
+
+extern void initPhysics(bool interactive);
+extern void stepPhysics(bool interactive);
+extern void cleanupPhysics(bool interactive);
+extern void keyPress(unsigned char key, const PxTransform& camera);
+
+
+namespace
+{
+	Snippets::Camera* sCamera;
+
+	void motionCallback(int x, int y)
+	{
+		sCamera->handleMotion(x, y);
+	}
+
+	void keyboardCallback(unsigned char key, int x, int y)
+	{
+		if (key == 27)
+			exit(0);
+
+		if (!sCamera->handleKey(key, x, y))
+			keyPress(key, sCamera->getTransform());
+	}
+
+	void mouseCallback(int button, int state, int x, int y)
+	{
+		sCamera->handleMouse(button, state, x, y);
+	}
+
+	void idleCallback()
+	{
+		glutPostRedisplay();
+	}
+
+	void renderCallback()
+	{
+		stepPhysics(true);
+
+		Snippets::startRender(sCamera->getEye(), sCamera->getDir());
+
+		PxScene* scene;
+		PxGetPhysics().getScenes(&scene, 1);
+		PxU32 nbActors = scene->getNbActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC);
+		if (nbActors)
+		{
+			std::vector<PxRigidActor*> actors(nbActors);
+			scene->getActors(PxActorTypeFlag::eRIGID_DYNAMIC | PxActorTypeFlag::eRIGID_STATIC, reinterpret_cast<PxActor**>(&actors[0]), nbActors);
+			Snippets::renderActors(&actors[0], static_cast<PxU32>(actors.size()), true);
+		}
+
+		Snippets::finishRender();
+	}
+
+	void exitCallback(void)
+	{
+		delete sCamera;
+		cleanupPhysics(true);
+
+	}
+}
+
+void renderLoop()
+{
+	sCamera = new Snippets::Camera(PxVec3(50.0f, 50.0f, 50.0f), PxVec3(-0.6f, -0.2f, -0.7f));
+
+	Snippets::setupDefaultWindow("PhysX Snippet HelloWorld");
+	Snippets::setupDefaultRenderState();
+
+	glutIdleFunc(idleCallback);
+	glutDisplayFunc(renderCallback);
+	glutKeyboardFunc(keyboardCallback);
+	glutMouseFunc(mouseCallback);
+	glutMotionFunc(motionCallback);
+	motionCallback(0, 0);
+
+	atexit(exitCallback);
+
+	initPhysics(false);
+	glutMainLoop();
+}
+#endif
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//  * Neither the name of NVIDIA CORPORATION nor the names of its
+//    contributors may be used to endorse or promote products derived
+//    from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+// OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Copyright (c) 2008-2021 NVIDIA Corporation. All rights reserved.
+// Copyright (c) 2004-2008 AGEIA Technologies, Inc. All rights reserved.
+// Copyright (c) 2001-2004 NovodeX AG. All rights reserved.  
+
+// ****************************************************************************
+// This snippet illustrates simple use of physx
+//
+// It creates a number of box stacks on a plane, and if rendering, allows the
+// user to create new stacks and fire a ball from the camera position
+// ****************************************************************************
+
+#include <ctype.h>
+
+#include "../snippets/snippetcommon/SnippetPrint.h"
+#include "../snippets/snippetcommon/SnippetPVD.h"
+#include "../snippets/snippetutils/SnippetUtils.h"
+
+using namespace physx;
+
+PxDefaultAllocator		gAllocator;
+PxDefaultErrorCallback	gErrorCallback;
+
+PxFoundation* gFoundation = NULL;
+PxPhysics* gPhysics = NULL;
+
+PxDefaultCpuDispatcher* gDispatcher = NULL;
+PxScene* gScene = NULL;
+
+PxMaterial* gMaterial = NULL;
+PxPvd* gPvd = NULL;
+
+PxReal stackZ = 10.0f;
+
+PxRigidDynamic* createDynamic(const PxTransform& t, const PxGeometry& geometry, const PxVec3& velocity = PxVec3(0))
+{
+	PxRigidDynamic* dynamic = PxCreateDynamic(*gPhysics, t, geometry, *gMaterial, 10.0f);
+	dynamic->setAngularDamping(0.5f);
+	dynamic->setLinearVelocity(velocity);
+	gScene->addActor(*dynamic);
+	return dynamic;
+}
+
+void createStack(const PxTransform& t, PxU32 size, PxReal halfExtent)
+{
+	PxShape* shape = gPhysics->createShape(PxBoxGeometry(halfExtent, halfExtent, halfExtent), *gMaterial);
+	for (PxU32 i = 0; i < size; i++)
+	{
+		for (PxU32 j = 0; j < size - i; j++)
+		{
+			PxTransform localTm(PxVec3(PxReal(j * 2) - PxReal(size - i), PxReal(i * 2 + 1), 0) * halfExtent);
+			PxRigidDynamic* body = gPhysics->createRigidDynamic(t.transform(localTm));
+			body->attachShape(*shape);
+			PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
+			gScene->addActor(*body);
+		}
+	}
+	shape->release();
+}
+
+void initPhysics(bool interactive)
+{
+	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
+
+	gPvd = PxCreatePvd(*gFoundation);
+	PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate(PVD_HOST, 5425, 10);
+	gPvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+
+	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, PxTolerancesScale(), true, gPvd);
+
+	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
+	sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
+	gDispatcher = PxDefaultCpuDispatcherCreate(2);
+	sceneDesc.cpuDispatcher = gDispatcher;
+	sceneDesc.filterShader = PxDefaultSimulationFilterShader;
+	gScene = gPhysics->createScene(sceneDesc);
+
+	PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
+	if (pvdClient)
+	{
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
+		pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
+	}
+	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
+
+	PxRigidStatic* groundPlane = PxCreatePlane(*gPhysics, PxPlane(0, 1, 0, 0), *gMaterial);
+	gScene->addActor(*groundPlane);
+
+	for (PxU32 i = 0; i < 6; i++)
+		createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 20, 2.0f);
+
+	if (!interactive)
+	{
+		//createDynamic(PxTransform(PxVec3(50.0f, 50.0f, 50.0f)), PxSphereGeometry(4.0f), PxVec3(-0.6f, -0.2f, -0.7f)*100);
+		for (PxU32 i = 1; i < 100; i++)
+			createDynamic(PxTransform(PxVec3(i, i, i)), PxSphereGeometry(1.0), PxVec3(-0.6f, -0.2f, -0.7f) * 100);
+	}
+}
+
+void stepPhysics(bool /*interactive*/)
+{
+	gScene->simulate(1.0f / 60.0f);
+	gScene->fetchResults(true);
+	PxU32 nbActors = gScene->getNbActors(PxActorTypeFlag::eRIGID_DYNAMIC);
+	if (nbActors) {
+		std::vector<PxRigidActor*> actors(nbActors);
+		gScene->getActors(PxActorTypeFlag::eRIGID_DYNAMIC, (PxActor**)&actors[0], nbActors);
+		for (PxU32 i = 0; i < nbActors; i++)
+		{
+			PxTransform tm = actors[i]->getGlobalPose();
+			if(tm.q.x * tm.q.y * tm.q.z != 0)
+				printf("id:%d\n transform : x[%2.1lf] y[%2.1lf] z[%2.1lf]\n quat : x[%2.1lf] y[%2.1lf] z[%2.1lf]\n\n ", i, tm.p.x, tm.p.y, tm.p.z, tm.q.x, tm.q.y, tm.q.z);
+		}
+	}
+}
+
+void cleanupPhysics(bool /*interactive*/)
+{
+	PX_RELEASE(gScene);
+	PX_RELEASE(gDispatcher);
+	PX_RELEASE(gPhysics);
+	if (gPvd)
+	{
+		PxPvdTransport* transport = gPvd->getTransport();
+		gPvd->release();	gPvd = NULL;
+		PX_RELEASE(transport);
+	}
+	PX_RELEASE(gFoundation);
+
+	printf("SnippetHelloWorld done.\n");
+}
+
+void keyPress(unsigned char key, const PxTransform& camera)
+{
+	switch (toupper(key))
+	{
+	case 'B':	createStack(PxTransform(PxVec3(0, 0, stackZ -= 10.0f)), 10, 2.0f);						break;
+	case 'Z':	cleanupPhysics(false); stackZ = 10.0f; initPhysics(false);								break;
+	case ' ':	createDynamic(camera, PxSphereGeometry(0.1), camera.rotate(PxVec3(0, 0, -1)) * 200);	break;
+	}
+}
+
+int main(int, const char* const*)
+{
+#ifdef RENDER_SNIPPET
+	extern void renderLoop();
+	renderLoop();
+#else
+	static const PxU32 frameCount = 10000;
+	initPhysics(false);
+	for (PxU32 i = 0; i < frameCount; i++)
+		stepPhysics(false);
+	cleanupPhysics(false);
+#endif
+
+	return 0;
+}
